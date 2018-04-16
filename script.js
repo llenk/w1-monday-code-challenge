555d58fbce0969ead15fb7da0d57a4ddd6578617
llenk <ellenkeal@gmail.com>
2018-04-16 13:22:04 -0500

    red background for step 5

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function doWhenReady() {
 
 function addDiv() {
     divCounter++;
-    $('#divOfDivs').append(`<div><p>${divCounter}</p>
+    $('#divOfDivs').append(`<div style="background:red"><p>${divCounter}</p>
         <button class="swapper">Swap</button>
         <button class="delete">Delete</button>
     </div>`);
