cad19b78dfc94be847fd2e1e981e016449347f3c
llenk <ellenkeal@gmail.com>
2018-04-16 13:38:35 -0500

    red and yellow background flips for steps 6 and 7

--- script.js
+++ script.js
@@ -8,8 +8,19 @@ function doWhenReady() {
 
 function addDiv() {
     divCounter++;
-    $('#divOfDivs').append(`<div style="background:red"><p>${divCounter}</p>
+    $('#divOfDivs').append(`<div style="background:red">
+        <p>${divCounter}</p>
         <button class="swapper">Swap</button>
         <button class="delete">Delete</button>
     </div>`);
+    $('.swapper:last').on('click', swapBackground);
+}
+
+function swapBackground() {
+    if (this.parentElement.style.background == 'red') {
+        this.parentElement.style.background = 'yellow';
+    }
+    else {
+        this.parentElement.style.background = 'red'
+    }
 }
